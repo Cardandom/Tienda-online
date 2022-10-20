4a33f6002dfb3bfcbd9db839643f2f514d914002
Aux inventario <auxinventario@grupoideasa.local>
2022-10-21 00:09:54 +0200

    adelantada clase de agragado de carrito de compras

--- script.js
+++ script.js
@@ -2,13 +2,31 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.burgerMenu');
 const mobileMenu = document.querySelector('.mobile-menu')
+const menuCarritoIcon = document.querySelector ('.navbar-shopping-cart')
+const aside = document.querySelector ('.product-detail');
+
 
 navEmail.addEventListener ('click',toggleDesktopMenu);
 burgerMenu.addEventListener ('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener ('click',toggleCarritoAside);
+
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu () {
+    const isAsideClosed = aside.classList.contains('inactive')
+
+    if(!isAsideClosed) {
+        aside.classList.add ('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive')
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+     aside.classList.toggle('inactive');
 }
