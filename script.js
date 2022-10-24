a626306b019053e33b77dc2deb28cf154d76d345
Carlos Daniel Dominguez <carlosddominguezc@gmail.com>
2022-10-23 21:27:25 -0500

    por fin listo ultimo de curso

--- script.js
+++ script.js
@@ -3,26 +3,80 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const burgerMenu = document.querySelector('.burgerMenu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarritoIcon = document.querySelector ('.navbar-shopping-cart');
-const aside = document.querySelector ('.product-detail');
+const productDetailCloseIcon = document.querySelector ('.product-detail-close');
+const shoppingCartContainer = document.querySelector ('#shoppingCartContainer');
+const productDetailContainer = document.querySelector ('#productDetail');
 const cardContainer = document.querySelector ('.cards-container');
 
 
 navEmail.addEventListener ('click',toggleDesktopMenu);
 burgerMenu.addEventListener ('click',toggleMobileMenu);
 menuCarritoIcon.addEventListener ('click',toggleCarritoAside);
+productDetailCloseIcon.addEventListener ('click',closeProductDetailAside);
 
+/**/
 
+function toggleDesktopMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive');
+    }
+
+    closeProductDetailAside();
+    
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+
+    if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive'); 
+    }
+
+    closeProductDetailAside();
+    
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+    }
+
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+    
+    if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive');
+    }
+    
+    shoppingCartContainer.classList.toggle('inactive');
+}
+
+function openProductDetailAside() {
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
+}
+
+/* 
 function toggleDesktopMenu () {
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
     if(!isAsideClosed) {
-        aside.classList.add ('inactive');
+        shoppingCartContainer.classList.add ('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu () {
-    const isAsideClosed = aside.classList.contains('inactive')
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive')
     if(!isAsideClosed) {
-        aside.classList.add ('inactive');
+        shoppingCartContainer.classList.add ('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -36,9 +90,23 @@ function toggleCarritoAside () {
     else if (!isNavEmailClosed) {
         desktopMenu.classList.add('inactive');
     }
-     aside.classList.toggle('inactive');
+    const isProductDetailClose = productDetailContainer.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+        productDetailContainer.classList.add('inactive');
+    }
+
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
+function openProductDetailAside () {
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside () {
+    productDetailContainer.classList.add('inactive');
+}
+*/
 const productList = [];
 productList.push ({
     name: 'Bike',
@@ -78,6 +146,7 @@ for (product of arr) {
 
     const productImg = document.createElement ('img');
     productImg.setAttribute('src',product.image);
+    productImg.addEventListener('click', openProductDetailAside);
 
     const productInfo = document.createElement ('div');
     productInfo.classList.add('product-info');
